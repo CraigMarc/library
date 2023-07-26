f80c9dfacc68496b9685afb43287482964936d2a
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-07-25 20:34:58 -0400

    updated js file

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 function Book(title, author, pages) {
+    console.log(this)
     this.title = title
     this.author = author
     this.pages = pages
@@ -6,8 +7,11 @@ function Book(title, author, pages) {
         return title + ' ' + author + ' ' + pages
   }
 }
-
+console.log(this)
 const theHobbit = new Book('The Hobbit' , 'Tolkien', '500')
 
 console.log(theHobbit.info())
-
+console.log(Object.getPrototypeOf(Book))
+console.log(Object.getPrototypeOf(theHobbit) == Book.prototype)
+console.log(Book.prototype)
+console.log(theHobbit.valueOf())
