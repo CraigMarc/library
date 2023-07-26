b65fcd8fa792bf1320de560c356a492ee3f89c66
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-07-25 21:00:52 -0400

    added table and addbook function

--- library.js
+++ library.js
@@ -1,5 +1,8 @@
+let myLibrary = [];
+
+
 function Book(title, author, pages) {
-    console.log(this)
+   
     this.title = title
     this.author = author
     this.pages = pages
@@ -7,11 +10,24 @@ function Book(title, author, pages) {
         return title + ' ' + author + ' ' + pages
   }
 }
-console.log(this)
-const theHobbit = new Book('The Hobbit' , 'Tolkien', '500')
 
+function addBookToLibrary(title, author, pages) {
+  myLibrary.push(new Book(title, author, pages))
+}
+
+function displayBooks(bookArray) {
+ 
+}
+
+
+addBookToLibrary('book3' , 'Tolkien', '500')
+console.log(myLibrary)
+
+const theHobbit = new Book('The Hobbit' , 'Tolkien', '500')
+console.log(theHobbit)
 console.log(theHobbit.info())
 console.log(Object.getPrototypeOf(Book))
 console.log(Object.getPrototypeOf(theHobbit) == Book.prototype)
 console.log(Book.prototype)
 console.log(theHobbit.valueOf())
+
