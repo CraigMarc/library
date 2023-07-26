fd5be66cc759b9b8675c2c7548d2fa31d6dde6de
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-07-26 18:36:57 -0400

    added event listner for delete button

--- library.js
+++ library.js
@@ -2,13 +2,13 @@ let myLibrary = [];
 
 
 function Book(title, author, pages, read) {
-   
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return title + ' ' + author + ' ' + pages
+
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function () {
+    return title + ' ' + author + ' ' + pages
   }
 }
 
@@ -20,40 +20,57 @@ function deleteTable(bookArray) {
   console.log(bookArray)
   for (let i = 1; i < bookArray.length + 1; i++) {
     document.getElementById("bookTable").deleteRow(1);
-    
-   
-    }
+
+
+  }
 }
 
 
 function displayBooks(bookArray) {
   for (let i = 0; i < bookArray.length; i++) {
-  let table = document.getElementById("bookTable");
-  let row = table.insertRow(1);
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-  let cell5 = row.insertCell(4);
+    let table = document.getElementById("bookTable");
+    let row = table.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
 
-  cell1.textContent = bookArray[i].title;
-  cell2.textContent = bookArray[i].author;
-  cell3.textContent = bookArray[i].pages;
-  cell4.textContent = bookArray[i].read;
-  let tBox = document.createElement('input');
-  tBox.setAttribute('type', 'checkbox');
-  tBox.setAttribute('value', 'i');
-  cell4.appendChild(tBox);
+    cell1.textContent = bookArray[i].title;
+    cell2.textContent = bookArray[i].author;
+    cell3.textContent = bookArray[i].pages;
+    cell4.textContent = bookArray[i].read;
+    let tBox = document.createElement('input');
+    tBox.setAttribute('type', 'checkbox');
+    tBox.setAttribute('value', 'i');
+    cell4.appendChild(tBox);
 
-  let deleteButton = document.createElement('button');
-  deleteButton.setAttribute('type', 'button');
-  deleteButton.setAttribute('value', 'i');
-  deleteButton.textContent = 'Delete'
-  cell5.appendChild(deleteButton);
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.setAttribute('id', 'deleteButton');
+    deleteButton.setAttribute('value', i);
+    deleteButton.textContent = 'Delete'
+    cell5.appendChild(deleteButton);
 
+  /*button event listener*/
+
+const number = document.querySelectorAll('#deleteButton');
+
+number.forEach((button) => {
+
+  button.addEventListener('click', () => {
+    let value = button.value
+
+console.log(value)
+
+    
+
+  })
+})
 
   }
 }
+
 
 
 /*
@@ -63,9 +80,9 @@ displayBooks(myLibrary)*/
 
 
 
- 
+
 /*get data from form*/
-  
+
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target).entries());
@@ -74,12 +91,15 @@ document.querySelector('form').addEventListener('submit', (e) => {
   let titleForm = data.title
   let readForm = data.read
   console.log(data)
-  if (myLibrary.length > 0)
-{ deleteTable(myLibrary)
-}
+  if (myLibrary.length > 0) {
+    deleteTable(myLibrary)
+  }
   addBookToLibrary(titleForm, authorForm, pagesForm, readForm)
   displayBooks(myLibrary)
   document.getElementById("form").reset();
+
+  
+
 });
 
 
